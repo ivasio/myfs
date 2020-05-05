ed69ddb33a7e74943230486f24e4816344281922
Ivan Chebotar <van.chebotar@yandex.ru>
2020-05-05 17:42:11 +0300

    ref: added server-side work with pipe

--- daemon/src/server.c
+++ daemon/src/server.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <unistd.h>
-
-// Usual socket headers
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include <web.h>
 #include <server.h>
 
 
+void resend_request(char *pipe_path, char *buf, char len);
+
 int main(void)
 {
     int server_port = 8005;
     int serverSocket = setup_server(server_port);
-    return run_server(serverSocket);
+
+    char* fifo_path = "/tmp/ivasio-fs-fifo";
+    setup_pipe(fifo_path);
+
+    return run_server(serverSocket, fifo_path);
+}
+
+void setup_pipe(char* path) {
+    // todo check if was created earlier
+    mkfifo(path, 0666);
 }
 
 int setup_server(int server_port) {
@@ -42,17 +50,17 @@ int setup_server(int server_port) {
     return serverSocket;
 }
 
-int run_server(socket_t server_socket) {
+int run_server(socket_t server_socket, char *pipe_path) {
     int serve_res;
     while(1) {
-        serve_res = serve(server_socket);
+        serve_res = serve(server_socket, pipe_path);
         if (serve_res != 0)
             return serve_res;
     }
 }
 
 
-int serve(socket_t server_socket) {
+int serve(socket_t server_socket, char *pipe_path) {
     socket_t client_socket = accept(server_socket, NULL, NULL);
     if (client_socket < 0) {
         printf("Error in accept!\n");
@@ -62,16 +70,10 @@ int serve(socket_t server_socket) {
     printf("New connection from client\n");
 
     web_request_t request;
-    read_request(&request, client_socket);
-    process_request(&request);
+    read_request(&request, client_socket, pipe_path);
 
     web_response_t response;
-    /* in case of invalid request
-    {
-    response->status = RESPONSE_INVALID_REQUEST;
-    }
-    */
-    construct_response(&request, &response);
+    construct_response(&response, pipe_path);
     send_response(&response, client_socket);
 
     // closing connection
@@ -86,7 +88,7 @@ int serve(socket_t server_socket) {
     return 0;
 }
 
-void read_request(web_request_t* request, socket_t client_socket) {
+void read_request(web_request_t *request, socket_t client_socket, char *pipe_path) {
     printf("Started reading request\n");
     char recv_buff[2];
 
@@ -100,11 +102,21 @@ void read_request(web_request_t* request, socket_t client_socket) {
 
     char* msg_buf = (char*) calloc(msg_len + 1, sizeof(char));
     read(client_socket, msg_buf, msg_len);
-    parse_request(request, msg_buf, msg_len);
+    resend_request(pipe_path, msg_buf, msg_len);
+    free(msg_buf);
+}
+
+void resend_request(char *pipe_path, char *buf, char len) {
+    int pipe = open(pipe_path, O_WRONLY);
+
+    write(pipe, &len, 1);
+    write(pipe, buf, len);
+
+    close(pipe);
 }
 
 
-int process_request(web_request_t *request) {
+int process_request(web_request_t *request, char *pipe_path) {
     printf("Got request for method id %d with %d args\n", request->operation_code, request->n_args);
     for (int i = 0; i < request->n_args; i++) {
         printf("%d : %s, ", i, request->args[i]);
@@ -125,7 +137,7 @@ int parse_request(web_request_t* request, char* receive_buffer, char buf_len) {
     request->args = (char**) calloc(n_args_total, sizeof(char*));
 
     char n_args = 0;
-    for(; (buff_pointer < receive_buffer + buf_len) && n_args < n_args_total; n_args++) {
+    for(; buff_pointer < receive_buffer + buf_len && n_args < n_args_total; n_args++) {
         request->args[n_args] = buff_pointer;
         buff_pointer += strlen(buff_pointer) + 1;  // moving to next 0-separated string
     }
@@ -135,15 +147,18 @@ int parse_request(web_request_t* request, char* receive_buffer, char buf_len) {
 }
 
 
-void construct_response(web_request_t* request, web_response_t* response) {
-    if (request == NULL) {
-        response->status = RESPONSE_INVALID_REQUEST;
-    } else {
-        response->status = RESPONSE_OK;
-        response->len = 1;
-        response->buff = (char*) calloc(2, sizeof(char));
-        sprintf(response->buff, "%d", request->operation_code);
-    }
+void construct_response(web_response_t *response, char *pipe_path) {
+    int pipe = open(pipe_path, O_RDONLY);
+
+    char status_len[2];
+    read(pipe, status_len, 2);
+    response->status = status_len[0];
+    response->len = status_len[1];
+
+    response->buff = (char*) calloc(status_len[1], sizeof(char));
+    read(pipe, response->buff, response->len);
+
+    close(pipe);
 }
 
 
